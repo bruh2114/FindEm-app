61c54da587209d307bbc32eb18a47609872aa718
mokgothu <refilwemonoketsi@gmail.com>
2022-10-17 12:04:58 +0200

    Settings

---
+++ components/Navigation.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import Constants from 'expo-constants';
+import { mdiNavigationVariantOutline } from '@mdi/js';
+
+import { Card } from 'react-native-paper';
+
+export default function Navigation() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        RECENT POSTS
+      </Text>
+            <Image style={styles.logo} source={require('../assets/images/women.png')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    
+  },
+});
